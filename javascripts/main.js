dd73d07a3c217a9976aea04c2a91cce85367b7ae
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-09 20:24:38 -0500

    nav bar

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log("working");
+var app = angular.module("TodoApp",[]);
+
+app.controller("NavCtrl",($scope) => { //name of the controller
+	$scope.cat=	"Meow";								
+	$scope.navItems = [{name:"logout"},{name:"All Item"},{name:"New Item"}];
+});
+
+app.controller("ItemCtrl",($scope) =>{
+	$scope.dog=	"woof";	
+
+});
+
