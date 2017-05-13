3cc9a34b2fdf14dec95626d3da443513d973d23d
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-13 12:26:18 -0500

    done

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,8 +10,8 @@ app.controller("NavCtrl", ($scope) => { //name of the controller
 });
 
 app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
-  //we use $q instead of rpomise 
-  //and we use $http instead of ajax 
+    //we use $q instead of rpomise 
+    //and we use $http instead of ajax 
     $scope.dog = "woof";
     $scope.showListView = true;
     $scope.items = [];
@@ -20,7 +20,6 @@ app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
     $scope.newItem = () => {
         console.log("newItem");
         $scope.showListView = false;
-
     };
 
     $scope.allItems = () => {
@@ -29,61 +28,60 @@ app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
     };
 
     let getItemList = () => {
-      let itemz = [];
-      return new $q((resolve, reject) => {
-        $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
-        .then((fbItems) => {
-          var itemCollection = fbItems.data;
-          Object.keys(itemCollection).forEach((key) => {
-            itemCollection[key].id=key;
-            itemz.push(itemCollection[key]);
-          });
-          resolve(itemz);
-        })
-        .catch((error) => {
-          reject(error);
+        let itemz = [];
+        return new $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+                .then((fbItems) => {
+                    var itemCollection = fbItems.data;
+                    Object.keys(itemCollection).forEach((key) => {
+                        itemCollection[key].id = key;
+                        itemz.push(itemCollection[key]);
+                    });
+                    resolve(itemz);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
         });
-      });
     };
-
 
 
     let getItems = () => {
-      getItemList()
-      .then((itemz) => {
-        console.log("itemz", itemz);
-        $scope.items=itemz;
-      })
-      .catch((error) => {
-        console.log("error in get", error);
-      });
+        getItemList()
+            .then((itemz) => {
+                console.log("itemz", itemz);
+                $scope.items = itemz;
+            })
+            .catch((error) => {
+                console.log("error in get", error);
+            });
     };
 
-getItems();
+    getItems();
 
-  let postNewItem = (newItem) => {
-    return $q((resolve,reject)=>{
-      $http.post(`${FIREBASE_CONFIG.databaseURL}/items.json` , JSON.stringify(newItem))
-      .then((resultz) => {
-        resolve(resultz);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-    });
-  };
+    let postNewItem = (newItem) => {
+        return $q((resolve, reject) => {
+            $http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
+                .then((resultz) => {
+                    resolve(resultz);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    };
 
-  $scope.addNewItem = () => {
-    $scope.newTask.isCompleted = false;
-      postNewItem($scope.newTask).then((response)=>{
-      $scope.newTask = {};
-      $scope.showListView = true;
-      getItems();
-    }).catch((error)=>{
-      console.log("add error",error);
-    });
+    $scope.addNewItem = () => {
+        $scope.newTask.isCompleted = false;
+        postNewItem($scope.newTask).then((response) => {
+            $scope.newTask = {};
+            $scope.showListView = true;
+            getItems();
+        }).catch((error) => {
+            console.log("add error", error);
+        });
 
-  };
+    };
 
 
 
