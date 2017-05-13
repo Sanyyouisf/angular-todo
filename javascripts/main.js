f3e2f193e2cead3bb3f5ca61571c2082bf50cf34
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-13 10:51:27 -0500

    add seeder

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-var app = angular.module("TodoApp",[]);
+
 
 app.controller("NavCtrl",($scope) => { //name of the controller
 	$scope.cat=	"Meow";								
@@ -8,26 +8,8 @@ app.controller("NavCtrl",($scope) => { //name of the controller
 app.controller("ItemCtrl",($scope) =>{
 	$scope.dog=	"woof";
 	$scope.showListView = true;
-	$scope.items = [
-        {
-          id: 0,
-          task: "mow the lawn",
-          isCompleted: true,
-          assignedTo: "Callan",
-        },
-        {
-          id: 1,
-          task: "grade quizzes",
-          isCompleted: false,
-          assignedTo: "Lauren",
-        },
-        {
-          id: 2,
-          task: "take a nap",
-          isCompleted: false,
-          assignedTo: "Zoe",
-        }
-      ];
+	$scope.items = [];
+
 
 
 
