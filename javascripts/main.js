fbf55d96a16a3228cbe12aba44ec5dedf5967132
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-13 12:18:05 -0500

    adding new item

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,6 +10,8 @@ app.controller("NavCtrl", ($scope) => { //name of the controller
 });
 
 app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+  //we use $q instead of rpomise 
+  //and we use $http instead of ajax 
     $scope.dog = "woof";
     $scope.showListView = true;
     $scope.items = [];
@@ -55,7 +57,31 @@ app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
       });
     };
 
-    getItems();
+    
+
+  let postNewItem = (newItem) => {
+    return $q((resolve,reject)=>{
+      $http.post(`${FIREBASE_CONFIG.databaseURL}/items.json` , JSON.stringify(newItem))
+      .then((resultz) => {
+        resolve(resultz);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+  };
+
+  $scope.addNewItem = () => {
+    $scope.newTask.isCompleted = false;
+      postNewItem($scope.newTask).then((response)=>{
+      $scope.newTask = {};
+      $scope.showListView = true;
+      getItems();
+    }).catch((error)=>{
+      console.log("add error",error);
+    });
+
+  };
 
 
 
