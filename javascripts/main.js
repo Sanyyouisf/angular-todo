7fb7be88f1f8e23d2a003db4057f5dd7623f8d87
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-13 11:43:10 -0500

    get working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,30 +1,70 @@
-
-
-app.controller("NavCtrl",($scope) => { //name of the controller
-	$scope.cat=	"Meow";								
-	$scope.navItems = [{name:"logout"},{name:"All Item"},{name:"New Item"}];
+app.run((FIREBASE_CONFIG) => { //
+    firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-app.controller("ItemCtrl",($scope) =>{
-	$scope.dog=	"woof";
-	$scope.showListView = true;
-	$scope.items = [];
+
+
+app.controller("NavCtrl", ($scope) => { //name of the controller
+    $scope.cat = "Meow";
+    $scope.navItems = [{ name: "logout" }, { name: "All Item" }, { name: "New Item" }];
+});
+
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+    $scope.dog = "woof";
+    $scope.showListView = true;
+    $scope.items = [];
+
+
+    $scope.newItem = () => {
+        console.log("newItem");
+        $scope.showListView = false;
+
+    };
+
+    $scope.allItems = () => {
+        console.log("allItem");
+        $scope.showListView = true;
+    };
+
+    let getItemList = () => {
+      let itemz = [];
+      return new $q((resolve, reject) => {
+        $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+        .then((fbItems) => {
+          var itemCollection = fbItems.data;
+          Object.keys(itemCollection).forEach((key) => {
+            itemCollection[key].id=key;
+            itemz.push(itemCollection[key]);
+          });
+          resolve(itemz);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+      });
+    };
+
+    let getItems = () => {
+      getItemList()
+      .then((itemz) => {
+        console.log("itemz", itemz);
+        $scope.items=itemz;
+      })
+      .catch((error) => {
+        console.log("error in get", error);
+      });
+    };
+
+    getItems();
 
 
 
 
-	$scope.newItem = () => {
-		console.log("newItem");
-		$scope.showListView = false;
 
-	};
 
-	$scope.allItems = () => {
-		console.log("allItem");
-		$scope.showListView = true;
-	};
+
+
 
 
 
 });
-
