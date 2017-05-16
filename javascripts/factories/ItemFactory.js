eb3fd6e5ca529b1aec26a88ac10563b34bd9ed41
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-16 18:39:59 -0500

    navbar

--- javascripts/factories/ItemFactory.js
+++ javascripts/factories/ItemFactory.js
@@ -3,13 +3,13 @@ app.factory("ItemFactory", function($q, $http, FIREBASE_CONFIG) {
         let itemz = [];
         return new $q((resolve, reject) => {
             $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
-                .then((fbItems) => {
-                    var itemCollection = fbItems.data;
-                    Object.keys(itemCollection).forEach((key) => {
-                        itemCollection[key].id = key;
-                        itemz.push(itemCollection[key]);
-                    });
-                    resolve(itemz);
+            .then((fbItems) => {
+                var itemCollection = fbItems.data;
+                Object.keys(itemCollection).forEach((key) => {
+                    itemCollection[key].id = key;
+                    itemz.push(itemCollection[key]);
+                });
+                resolve(itemz);
                 })
                 .catch((error) => {
                     reject(error);
@@ -20,12 +20,12 @@ app.factory("ItemFactory", function($q, $http, FIREBASE_CONFIG) {
     let postNewItem = (newItem) => {
         return $q((resolve, reject) => {
             $http.post(`${FIREBASE_CONFIG.databaseURL}/items.json`, JSON.stringify(newItem))
-                .then((resultz) => {
-                    resolve(resultz);
-                })
-                .catch((error) => {
+            .then((resultz) => {
+                resolve(resultz);
+            })
+            .catch((error) => {
                     reject(error);
-                });
+            });
         });
     };
 
