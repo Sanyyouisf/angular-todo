444250fd3ab80afd92d243bf8299dbeb07d526b8
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-22 20:02:44 -0500

    login and register

---
+++ javascripts/controllers/AuthCtrl.js
@@ -0,0 +1,53 @@
+app.controller("AuthCtrl", function($rootScope, $location,$scope, AuthFactory, UserFactory) {
+    console.log("inside AuthCtrl ");
+
+    $scope.auth = {
+    	email:"s@s.com",
+    	password:"111111"
+    };
+
+    let LogMeIn=()=>{
+    	AuthFactory.authenticate($scope.auth)
+    	.then((usercreds)=>{
+    		console.log("usercred",usercreds);
+    		return UserFactory.getUser(usercreds.uid);
+    	},(error)=>{
+    		console.log("error in Authenticate",error);
+
+    	}).then((user)=>{
+    		console.log("user",user);
+    		$rootScope.user = user;
+    		$location.url('items/list');
+    	}).catch((error)=>{
+    		console.log("getUser error", error );
+    	});
+    };
+
+    $scope.registerUser = () => {
+    	//new auth
+    	
+    	AuthFactory.registerWithEmail($scope.auth)
+    	.then((didRegister)=>{
+    		console.log("you registered ", didRegister);
+    		$scope.auth.uid = didRegister.uid;
+    		return UserFactory.addUser($scope.auth);
+    		// console.log("$scope.auth inside registerUser",$scope.auth);
+
+    	}, (error) => {
+    		console.log("error in registerWithEmail",error);
+    	}).then((registerComplete)=>{
+    		console.log("registerComplete",registerComplete);
+    		LogMeIn();
+    	}).catch((error)=>{
+    		console.log("error",error);
+    	});
+    	//adding user name
+    	//log in 
+
+    };
+
+    $scope.loginUser = () => {
+    	LogMeIn();
+    };
+
+});
